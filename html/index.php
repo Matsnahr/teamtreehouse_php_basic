ac1c4f59a2e0f79b29e30e409a420605c2fc8a9e
matsnahr <matsnahr01@gmail.com>
2020-09-24 11:46:37 +0700

    update

--- html/index.php
+++ html/index.php
@@ -6,8 +6,8 @@ $name = "Matsna Fams";
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="html/asset/css/bootstrap.min.css">
-    <link rel="stylesheet" href="html/asset/css/all.css">
+    <link rel="stylesheet" href="asset/css/bootstrap.min.css">
+    <link rel="stylesheet" href="asset/css/all.css">
     <style>
         
     </style>
